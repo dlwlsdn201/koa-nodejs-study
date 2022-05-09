2cd7e7f621881b66fd125e62e96df058f58d3850
jinWoo_Lee <dlwlsdn201@naver.com>
2022-05-09 22:21:17 +0900

    nodemon 도구 설치 및 사용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
+// // 라우터 설정
+// router.get('/', ctx => {
+//   ctx.body = '홈';
+// });
+
+// router.get('/about', ctx => {
+//   ctx.body = '소개';
+// });
+
+// // app 인스턴스에 라우터 적용
+// app.use(router.routes()).use(router.allowedMethods());
+
+// app.listen(4000, () => {
+//   console.log('Listening to port 4000');
+// })
+
+// ---------nodemon 사용하기---------
 app.use(async (ctx, next) => {
-  ctx.body = 'hello world';
   console.log(ctx.url);
   console.log(1);
   if (ctx.query.authorized !== '1') {
@@ -20,6 +38,10 @@ app.use(async (ctx, next) => {
 app.use((ctx, next) => {
   console.log(2);
   next();
+});
+
+app.use((ctx) => {
+  ctx.body = 'hello world';
 });
 
 app.listen(4000, () => {
